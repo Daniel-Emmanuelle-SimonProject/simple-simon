69630f8d8e63d12ca3a7010313d444a0ac6f0c4d
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-07-11 10:56:18 -0500

    Added function to attach event listners)

--- public/js/main.js
+++ public/js/main.js
@@ -5,44 +5,46 @@
 var sequence = [];
 var iteration = 0;
 var count = 0;
+var doc = $(document)
 
+/************************ GAME FUNCTIONS ******************/
+
+function startGame(){
+	randomNumber();
+	setTimeout(attachEvents,2000)
+}
 //random number generator from 1-4
 function randomNumber(){
 	var number = parseInt(Math.random() * (5 - 1) + 1);
 	sequence.push(number);
-  console.log(sequence);
 	boxSelected(sequence);
 };
-
-randomNumber();
-
-
  // runs through sequence array and changes opacity of boxes determined by number value
 function boxSelected(){
 	var changeOpacity = setInterval(function(){
 		if (count < sequence.length) {
 				if (sequence[count] == 1) {
-					$('#red').css('opacity','0')
+					$('#head').css('opacity','0')
 					setTimeout(function(){
-						$('#red').css('opacity','1')
+						$('#head').css('opacity','1')
 					},1000);
 					count++;
 				}else if (sequence[count] == 2) {
-					$('#blue').css('opacity','0')
+					$('#leftArm').css('opacity','0')
 					setTimeout(function(){
-						$('#blue').css('opacity','1')
+						$('#leftArm').css('opacity','1')
 					},1000);
 					count++;
 				}else if (sequence[count] == 3) {
-					$('#green').css('opacity','0')
+					$('#rightArm').css('opacity','0')
 					setTimeout(function(){
-						$('#green').css('opacity','1')
+						$('#rightArm').css('opacity','1')
 					},1000);
 					count++
 				}else if (sequence[count] == 4) {
-					$('#yellow').css('opacity','0')
+					$('#abs').css('opacity','0')
 					setTimeout(function(){
-						$('#yellow').css('opacity','1')
+						$('#abs').css('opacity','1')
 					},1000);
 					count++
 				}
@@ -69,67 +71,73 @@ function compare(input){
   }
 }
 
+/******************** EVENT LISTENERS *****************/
 //assigning the numbers to each keypress
-//up (1)
-$(document).on("keydown", function(e){
-	if(e.keyCode == 38){
-    console.log(e.keyCode);
-		$("#red").css("opacity", "0.75");
-    iteration++
-	};
-});
-$(document).on("keyup", function(e){
-	if(e.keyCode == 38){
-		$("#red").css("opacity", "1");
-    compare(1);
-	};
-});
 
-//left (2)
-$(document).on("keydown", function(e){
-	if(e.keyCode == 37){
-    console.log(e.keyCode);
-		$("#blue").css("opacity", "0.75");
-    iteration++
-	};
-});
-$(document).on("keyup", function(e){
-	if(e.keyCode == 37){
-		$("#blue").css("opacity", "1");
-    compare(2);
-	};
-});
+function attachEvents(){
+	doc.on('keydown',upArrow)
+	doc.on('keyup',upArrow)
 
-//right (3)
-$(document).on("keydown", function(e){
-	if(e.keyCode == 39){
-    console.log(e.keyCode);
-		$("#green").css("opacity", "0.75");
-    iteration++
-	};
-});
-$(document).on("keyup", function(e){
-	if(e.keyCode == 39){
-		$("#green").css("opacity", "1");
-    compare(3);
-	};
-});
+	doc.on('keydown',leftArrow)
+	doc.on('keyup',leftArrow)
 
+	doc.on('keydown',rightArrow)
+ 	doc.on('keyup',rightArrow)
 
-//down (4)
-$(document).on("keydown", function(e){
-	if(e.keyCode == 40){
-    console.log(e.keyCode);
-		$("#yellow").css("opacity", "0.75");
-    iteration++;
-	};
-});
-$(document).on("keyup", function(e){
-	if(e.keyCode == 40){
-		$("#yellow").css("opacity", "1");
-    compare(4);
-	};
-});
+	doc.on('keydown',downArrow)
+	doc.on('keyup',downArrow)
+}
+
+function upArrow(e){
+		if(e.keyCode == 38){
+			console.log(e.keyCode);
+			$("#head").css("opacity", "0.75");
+			iteration++
+		};
+		if(e.keyCode == 38){
+			$("#head").css("opacity", "1");
+			compare(1);
+		};
+}
+
+function leftArrow(e){
+
+		if(e.keyCode == 37){
+			console.log(e.keyCode);
+			$("#leftArm").css("opacity", "0.75");
+			iteration++
+		};
+		if(e.keyCode == 37){
+			$("#leftArm").css("opacity", "1");
+			compare(2);
+		};
+}
+
+function rightArrow(e){
+
+		if(e.keyCode == 39){
+			console.log(e.keyCode);
+			$("#rightArm").css("opacity", "0.75");
+			iteration++
+		};
+		if(e.keyCode == 39){
+			$("#rightArm").css("opacity", "1");
+			compare(3);
+		};
+}
+
+function downArrow(e){
+
+		if(e.keyCode == 40){
+			console.log(e.keyCode);
+			$("#abs").css("opacity", "0.75");
+			iteration++;
+		};
+		if(e.keyCode == 40){
+			$("#abs").css("opacity", "1");
+			compare(4);
+		};
+}
 
 
 
